f924a7044ec531d290d8df36c12be704376ef119
cedis81 <cedisweb0@gmail.com>
2022-02-02 17:22:31 -0500

    Remove recursive solution for 344.js

--- array-patterns-one/344.js
+++ array-patterns-one/344.js
@@ -19,22 +19,3 @@
     }
     // nothing to return
 };
-
-// recursive solution
-
-/**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
- */
- var reverseString = function(s) {
-    function helper(left, right) {
-    //base case if left >= right
-        if (left >= right) return;
-    //swap characters        
-        [s[left], s[right]] = [s[right], s[left]];
-    //recursive call
-        return helper(left +1, right - 1);
-    }
-    //initial call
-    helper(0, s.length-1);
-};
